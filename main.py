123c761d5f7300513385c594a1c4b1d56c358e56
Beannsss <bpwhelan95@gmail.com>
2022-09-10 17:49:55 -0400

    Remove Pycharm Garbage and refactor

--- main.py
+++ main.py
@@ -1,29 +1,21 @@
-# This is a sample Python script.
 import datetime
 import os
 import shutil
-
-from dateutil import parser
 import pytz
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-input = './input/'
-output = './output/'
+input_dir = './input/'
+output_dir = './output/'
 kr = pytz.timezone('Asia/Seoul')
 utc = pytz.utc
 
 
-def print_hi(name):
-    for root, dirs, files in os.walk(input):
+def fix_date():
+    for root, dirs, files in os.walk(input_dir):
         for file in files:
             dt = datetime.datetime.strptime(file, 'PXL_%Y%m%d_%H%M%S%f.mp4').replace(tzinfo=datetime.timezone.utc)
             kr_dt_str = dt.astimezone(kr).strftime('PXL_%Y%m%d_%H%M%S%f.mp4').replace('000.mp4', '.mp4')
-            shutil.copy2(input + file, output + kr_dt_str)
+            shutil.copy2(input_dir + file, output_dir + kr_dt_str)
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    fix_date()
