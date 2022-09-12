133d5595faa4b519c882e59206fecdb08e79a4e6
Beans <bpwhelan95@gmail.com>
2022-09-12 10:28:49 -0400

    Refactor and requirements.txt

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 import datetime
 import os
-import shutil
 import pytz
+import shutil
 
 input_dir = './input/'
 output_dir = './output/'
-kr = pytz.timezone('Asia/Seoul')
-utc = pytz.utc
+
+# Three Lines to Change Possibly
+pxl_format = 'PXL_%Y%m%d_%H%M%S%f.mp4'
+to_tz = pytz.timezone('Asia/Seoul')
+from_tz = pytz.utc
+
+file_extension = pxl_format.rsplit('.', 1)[1]
 
 
 def fix_date():
     for root, dirs, files in os.walk(input_dir):
         for file in files:
-            dt = datetime.datetime.strptime(file, 'PXL_%Y%m%d_%H%M%S%f.mp4').replace(tzinfo=datetime.timezone.utc)
-            kr_dt_str = dt.astimezone(kr).strftime('PXL_%Y%m%d_%H%M%S%f.mp4').replace('000.mp4', '.mp4')
+            dt = datetime.datetime.strptime(file, pxl_format).replace(tzinfo=from_tz)
+            kr_dt_str = dt.astimezone(to_tz).strftime(pxl_format).replace('000.', '.')
             shutil.copy2(input_dir + file, output_dir + kr_dt_str)
 
 
